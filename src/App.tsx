cc9a80c993eccff1600d78e264c453a8f372979c
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-24 09:41:52 +0100

    Add choose Operator function

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 import { useState } from "react";
 function App() {
   const [firstOperand, setFirstOperand] = useState("0");
-  const [secondOperand, setSecondOperand] = useState('');
+  const [secondOperand, setSecondOperand] = useState();
 
   const operations = [".", "/", "+", "-", "*"];
-  const equals = "="
+  const equals = "=";
 
   const choose = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     const value = (e.target as HTMLButtonElement).id;
@@ -14,106 +14,117 @@ function App() {
       value.startsWith(operation)
     );
 
-    if(operations.includes(firstOperand.slice(-1))){
-      console.log(firstOperand)
-      return
+    if (operations.includes(firstOperand.slice(-1))) {
+      console.log(firstOperand);
+      return;
     }
     // if (operations.some(operation => firstOperand.includes(operation))) {
     //   console.log(firstOperand);
     //   return;
     // }
-    
 
     if (isFirstValueOperation) {
       setFirstOperand(firstOperand + value);
     }
   };
 
-  const chooseOperator = () => {
+  const chooseOperator = (e) => {
+    const value = e.target.id;
+    console.log(value);
+    
+    if (operations.some(operation => firstOperand.includes(operation))) return;
+    if (firstOperand === "0") {
+      setFirstOperand(prev => prev + value);
+    }
+  };
+  
+  
+  
 
-  }
 
+  const chooseEquals = () => {};
 
-    // if (firstOperand.startsWith("0") && firstOperand.length === 1) {
-    //   console.log("odje 2");
-    //   setFirstOperand("");
-    //   setFirstOperand((prev) => prev + value);
-    // }
-    // const isFirstDigit =
-    //   firstOperand === "" && ["+", "-", "*", "/", "="].indexOf(value) === -1;
-    // const isStartsWithZero =
-    //   firstOperand === "0" && ["+", "-", "*", "/", "="].indexOf(value) === -1;
-    // console.log(isFirstDigit)
-    // console.log(isStartsWithZero)
+  // if (firstOperand.startsWith("0") && firstOperand.length === 1) {
+  //   console.log("odje 2");
+  //   setFirstOperand("");
+  //   setFirstOperand((prev) => prev + value);
+  // }
+  // const isFirstDigit =
+  //   firstOperand === "" && ["+", "-", "*", "/", "="].indexOf(value) === -1;
+  // const isStartsWithZero =
+  //   firstOperand === "0" && ["+", "-", "*", "/", "="].indexOf(value) === -1;
+  // console.log(isFirstDigit)
+  // console.log(isStartsWithZero)
 
-    // setFirstOperand(prev => {
-    //   if(prev.startsWith("0")){
-    //     return prev = value
-    //   } else {
-    //     return prev += value
-    //   }
-    // })
+  // setFirstOperand(prev => {
+  //   if(prev.startsWith("0")){
+  //     return prev = value
+  //   } else {
+  //     return prev += value
+  //   }
+  // })
 
   return (
     <>
       <div className="calculator">
         <div className="display">
-          <div className="secondOperand">{secondOperand}</div>
+          <div className="calculations">{secondOperand}</div>
           <div className="resultDiv">{firstOperand}</div>
         </div>
+
         <div className="clearDelete">
-          <button className="clear">CLEAR</button>
-          <button className="delete">DELETE</button>
+          <div className="clear">CLEAR</div>
+          <div className="delete">DELETE</div>
         </div>
         <div className="buttons">
-          <button id="7" onClick={(e) => choose(e)} className="number7 btn">
+          <div id="7" onClick={choose} className="number7 btn">
             7
-          </button>
-          <button id="8" onClick={choose} className="number8 btn">
+          </div>
+          <div id="8" onClick={choose} className="number8 btn">
             8
-          </button>
-          <button id="9" onClick={choose} className="number9 btn">
+          </div>
+          <div id="9" onClick={choose} className="number9 btn">
             9
-          </button>
-          <button id="/" onClick={choose} className="divide btn">
+          </div>
+          <div id="/" onClick={chooseOperator} className="divide btn">
             /
-          </button>
-          <button id="4" onClick={choose} className="number4 btn">
+          </div>
+          <div id="4" onClick={choose} className="number4 btn">
             4
-          </button>
-          <button id="5" onClick={choose} className="number5 btn">
+          </div>
+          <div id="5" onClick={choose} className="number5 btn">
             5
-          </button>
-          <button id="6" onClick={choose} className="number6 btn">
+          </div>
+          <div id="6" onClick={choose} className="number6 btn">
             6
-          </button>
-          <button id="*" onClick={choose} className="multiply btn">
+          </div>
+          <div id="*" onClick={chooseOperator} className="multiply btn">
             *
-          </button>
-          <button id="1" onClick={choose} className="number1 btn">
+          </div>
+          <div id="1" onClick={choose} className="number1 btn">
             1
-          </button>
-          <button id="2" onClick={choose} className="number2 btn">
+          </div>
+          <div id="2" onClick={choose} className="number2 btn">
             2
-          </button>
-          <button id="3" onClick={choose} className="number3 btn">
+          </div>
+          <div id="3" onClick={choose} className="number3 btn">
             3
-          </button>
-          <button id="-" onClick={choose} className="minus btn">
+          </div>
+          <div id="-" onClick={chooseOperator} className="minus btn">
             -
-          </button>
-          <button id="0" onClick={choose} className="number0 btn">
+          </div>
+          <div id="0" onClick={choose} className="number0 btn">
             0
-          </button>
-          <button id="." onClick={choose} className="dot btn">
+          </div>
+          <div id="." onClick={chooseOperator} className="dot btn">
             .
-          </button>
-          <button id="=" onClick={choose} className="equals btn">
+          </div>
+          <div id="=" onClick={chooseEquals} className="equals btn">
             =
-          </button>
-          <button id="+" onClick={choose} className="plus btn">
+          </div>
+          <div id="+" onClick={chooseOperator} className="plus btn">
             +
-          </button>
+          </div>
         </div>
       </div>
     </>
