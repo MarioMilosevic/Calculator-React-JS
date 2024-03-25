3879b481e85d57aa92fef84e603f14e3793b3958
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-25 13:09:46 +0100

    Refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { useState } from "react";
+
 function App() {
   const [firstOperand, setFirstOperand] = useState("0");
   const [secondOperand, setSecondOperand] = useState("");
+  const [operation, setOperation] = useState("");
 
-  const operations = ["/", "+", "-", "*"];
-
-  const choose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const chooseNumber = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const value = (e.target as HTMLButtonElement).id;
 
     if (firstOperand === "0") {
@@ -17,39 +17,28 @@ function App() {
 
   const chooseOperator = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const value = (e.target as HTMLButtonElement).id;
-    if (operations.some((operation) => secondOperand.includes(operation))) {
-      return;
-    }
-    const { a, b, operation } = getInputs();
 
-    if (a && b && operation) {
-      const result = String(calculateResult(a, b, operation));
+    if (firstOperand && secondOperand && operation) {
+      const result = String(
+        calculateResult(firstOperand, secondOperand, operation)
+      );
       setSecondOperand(result + value);
       setFirstOperand("");
+      setOperation("");
     } else {
-      setSecondOperand(firstOperand + value);
+      setSecondOperand(firstOperand);
       setFirstOperand("");
+      setOperation(value);
     }
-  };
-
-  const getInputs = () => {
-    const a = firstOperand;
-    const bAndOperation = secondOperand;
-    const b = bAndOperation.slice(0, -1);
-    const operation = bAndOperation.slice(-1);
-    const inputs = {
-      a,
-      b,
-      operation,
-    };
-    return inputs;
   };
 
   const chooseEquals = () => {
-    const { a, b, operation } = getInputs();
-    const result = String(calculateResult(a, b, operation));
+    const result = String(
+      calculateResult(firstOperand, secondOperand, operation)
+    );
     setFirstOperand(result);
     setSecondOperand("");
+    setOperation("");
   };
 
   const calculateResult = (
@@ -85,7 +74,7 @@ function App() {
       setFirstOperand("0" + value);
     } else if (!firstOperand.includes(".")) {
       setFirstOperand((prev) => prev + value);
-    } 
+    }
   };
 
   const clearOperands = () => {
@@ -106,7 +95,10 @@ function App() {
     <>
       <div className="calculator">
         <div className="display">
-          <div className="calculations">{secondOperand}</div>
+          <div className="calculations">
+            {secondOperand}
+            {operation}
+          </div>
           <div className="resultDiv">{firstOperand}</div>
         </div>
 
@@ -119,43 +111,43 @@ function App() {
           </div>
         </div>
         <div className="buttons">
-          <div id="7" onClick={choose} className="number7 btn">
+          <div id="7" onClick={chooseNumber} className="number7 btn">
             7
           </div>
-          <div id="8" onClick={choose} className="number8 btn">
+          <div id="8" onClick={chooseNumber} className="number8 btn">
             8
           </div>
-          <div id="9" onClick={choose} className="number9 btn">
+          <div id="9" onClick={chooseNumber} className="number9 btn">
             9
           </div>
           <div id="/" onClick={(e) => chooseOperator(e)} className="divide btn">
             /
           </div>
-          <div id="4" onClick={choose} className="number4 btn">
+          <div id="4" onClick={chooseNumber} className="number4 btn">
             4
           </div>
-          <div id="5" onClick={choose} className="number5 btn">
+          <div id="5" onClick={chooseNumber} className="number5 btn">
             5
           </div>
-          <div id="6" onClick={choose} className="number6 btn">
+          <div id="6" onClick={chooseNumber} className="number6 btn">
             6
           </div>
           <div id="*" onClick={chooseOperator} className="multiply btn">
             *
           </div>
-          <div id="1" onClick={choose} className="number1 btn">
+          <div id="1" onClick={chooseNumber} className="number1 btn">
             1
           </div>
-          <div id="2" onClick={choose} className="number2 btn">
+          <div id="2" onClick={chooseNumber} className="number2 btn">
             2
           </div>
-          <div id="3" onClick={choose} className="number3 btn">
+          <div id="3" onClick={chooseNumber} className="number3 btn">
             3
           </div>
           <div id="-" onClick={chooseOperator} className="minus btn">
             -
           </div>
-          <div id="0" onClick={choose} className="number0 btn">
+          <div id="0" onClick={chooseNumber} className="number0 btn">
             0
           </div>
           <div id="." onClick={chooseDot} className="dot btn">
